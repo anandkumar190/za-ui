98d3f00b961ecebca416c6639eee35395b7e8f99
Anand <anandkumar.kumar190@gmail.com>
2026-06-15 23:40:41 +0530

    fixed

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'za-user');
+define('DB_PASS', 'Anand@123!');
 define('DB_NAME', 'zaviora_db');
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
