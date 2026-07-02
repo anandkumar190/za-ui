cc11232edf823a809c72645faa09bc85ffbb0261
Anand <anandkumar.kumar190@gmail.com>
2026-07-02 11:59:02 +0530

    upadte fb pixel

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
 
 <!-- Meta Pixel Code -->
 <!-- Meta Pixel Code -->
+<!-- Meta Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -40,12 +41,13 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1019919591030716');
+fbq('init', '1030373859409592');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1019919591030716&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=1030373859409592&ev=PageView&noscript=1"
 /></noscript>
+<!-- End Meta Pixel Code -->
 <!-- End Meta Pixel Code -->
 <!-- End Meta Pixel Code -->
 
